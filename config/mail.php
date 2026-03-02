ad9a40c7458281539f8b90a81b414dadf95853bc
gobinathgraspsoftware-dot <gobinath.graspsoftware@gmail.com>
2026-03-02 14:39:39 +0530

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 587),
+            'port' => env('MAIL_PORT', 465),
             'username' => env('MAIL_USERNAME', 'gobinath.graspsoftware@gmail.com'),
             'password' => env('MAIL_PASSWORD', 'znncrokvbaskzllg'),
             'timeout' => null,
